717b5a537b38950391a8537f2ba979d2da60113a
rony ginosar <rony.ginosar@mail.huji.ac.il>
2020-03-04 17:44:23 +0200

    c particles and vehicles

--- C/sketch.js
+++ C/sketch.js
@@ -1,7 +1,41 @@
+let vehicles = [];
+var font;
+let textSize = 400;
+
+function preload() {
+	font = loadFont('assets/Caladea-Bold.ttf');
+}
+
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight);
+	textFont(font);
+
+	// C letter
+	// stroke(255);
+	// strokeWeight(1);
+	// noFill();
+	// textFont(font);
+	// textSize(800);
+	// text('C', 100, 700);
+
+	// C letter shape
+	cbounds = font.textToPoints('C', windowWidth/4, windowHeight*2/3, textSize);
+	for (var i = 0 ; i < cbounds.length ; i++){
+		var b = cbounds[i];
+		var vehicle = new Vehicle(b.x, b.y);
+		vehicles.push(vehicle);
+		// stroke(255);
+		// strokeWeight(4);
+		// point(b.x, b.y);
+	}
 }
 
 function draw() {
-
+  background(0);
+	for (var i = 0 ; i < vehicles.length ; i++){
+		var v = vehicles[i];
+		v.behaviors();
+		v.update();
+		v.show();
+	}
 }
---
+++ C/vehicles.js
@@ -0,0 +1,100 @@
+// https://www.youtube.com/watch?v=4hA7G3gup-4&vl=en
+// nature of code - dan shiffman
+// steering = desired - current velocity
+
+// todo - group behaviors?
+
+
+let particlesize = 4;
+var fleecondition = 50;
+
+function Vehicle(x,y){
+  this.pos = createVector(random(width),random(height));
+  this.target = createVector(x,y);
+  // this.vel = createVector();
+  this.vel = p5.Vector.random2D();
+  this.acc = createVector();
+  this.r = particlesize;
+  // this.maxspeed = 5; // desired vel is always max speed
+  // this.maxforce = 0.3;
+  this.maxspeed = 10; // desired vel is always max speed
+  this.maxforce = 1;
+}
+
+Vehicle.prototype.behaviors = function(){
+  // var seek = this.seek(this.target);
+  // this.applyForce(seek);
+  var arrive = this.arrive(this.target);
+  var mouse = createVector(mouseX,mouseY);
+  var flee = this.flee(mouse);
+
+  // weights
+  arrive.mult(1);
+  flee.mult(5);
+
+  this.applyForce(arrive);
+  this.applyForce(flee);
+
+  // todo Add Jitter
+}
+
+// add forces to acceleration, accumulates
+Vehicle.prototype.applyForce = function(f){
+  this.acc.add(f);
+}
+
+// todo read more about prototype
+Vehicle.prototype.update = function(){
+  this.pos.add(this.vel);
+  this.vel.add(this.acc);
+  // clear acceleration
+  this.acc.mult(0);
+}
+
+Vehicle.prototype.show = function(){
+  stroke(255);
+  strokeWeight(particlesize);
+  point(this.pos.x, this.pos.y);
+}
+
+// pure 'seek' alg
+Vehicle.prototype.seek = function(target){
+  // get a vector, from the object, to the thing it's seeking
+  var desired = p5.Vector.sub(target, this.pos);
+  desired.setMag(this.maxspeed);
+  var steer = p5.Vector.sub(desired, this.vel);
+  steer.limit(this.maxforce); // NOTE: awesome function!
+  return steer;
+}
+
+// 'arrive' alg , go to the target, not fast as possible
+// slower if i'm closer... (no over-shooting)
+Vehicle.prototype.arrive = function(target){
+  // get a vector, from the object, to the thing it's seeking
+  var desired = p5.Vector.sub(target, this.pos);
+  var d = desired.mag();
+  var speed = this.maxspeed;
+  if (d < 100){
+    speed = map(d, 0, 100, 0, this.maxspeed);
+  }
+  desired.setMag(speed);
+  var steer = p5.Vector.sub(desired, this.vel);
+  steer.limit(this.maxforce); // NOTE: awesome function!
+  return steer;
+}
+
+// pure 'seek' alg, but oposite direction from mouse
+Vehicle.prototype.flee = function(target){
+  // get a vector, from the object, to the thing it's seeking
+  var desired = p5.Vector.sub(target, this.pos);
+  var d = desired.mag();
+  if (d < fleecondition){
+    desired.setMag(this.maxspeed);
+    desired.mult(-1);
+    var steer = p5.Vector.sub(desired, this.vel);
+    steer.limit(this.maxforce);
+    return steer;
+  } else {
+    return createVector(0,0);
+  }
+}
